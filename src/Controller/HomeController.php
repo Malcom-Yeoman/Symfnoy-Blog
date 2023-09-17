e5bba1c54d10a9e0448867c6ad8fcfbcd1dbaf42
Malcom Yeoman <malcomyeoman@air-de-malcom.home>
2023-09-17 19:06:40 +0200

    filter by category

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -13,36 +13,53 @@ use Symfony\Component\HttpFoundation\Request;
 class HomeController extends AbstractController
 {
     #[Route('/', name: 'app_home')]
-    public function index(Request $request,PostRepository $postRepository, CategoryRepository $categoryRepository, PaginatorInterface $paginator): Response
+    public function index(Request $request, PostRepository $postRepository, CategoryRepository $categoryRepository, PaginatorInterface $paginator): Response
     {
-        $query = $postRepository->createQueryBuilder('a')
-            ->orderBy('a.published_date', 'DESC')  // Sort articles by publication date
-            ->getQuery();
-
+        // Retrieve the category ID if provided
+        $categoryId = $request->query->get('category'); 
+    
+        // Create a query to fetch posts, sorted by published date in descending order
+        $queryBuilder = $postRepository->createQueryBuilder('a')->orderBy('a.published_date', 'DESC');
+    
+        // If a category ID is provided, add a condition to the query
+        if ($categoryId) {
+            $queryBuilder->andWhere('a.category = :categoryId')
+                         ->setParameter('categoryId', $categoryId);
+        }
+    
+        // Finalize the query
+        $query = $queryBuilder->getQuery();
+    
+        // Use paginator to paginate the results
         $posts = $paginator->paginate(
             $query,
-            $request->query->getInt('page', 1), // Gets the page number from the query, default 1
-            4 // Number of articles per page
+            $request->query->getInt('page', 1), // Retrieve the page number from the query, defaulting to 1 if not provided
+            3 // Number of posts per page
         );
-
-        // Fetch all categories
+    
+        // Retrieve all available categories
         $categories = $categoryRepository->findAll();
-
+    
+        // Render the view with the paginated posts and available categories
         return $this->render('home/index.html.twig', [
             'posts' => $posts,
-            'categories' => $categories
+            'categories' => $categories,
+            'selectedCategory' => $categoryId
         ]);
-    }
+    }    
 
     #[Route('/post/{id}', name: 'app_show_post')]
     public function show(int $id, PostRepository $postRepository): Response
     {
+        // Find the post by its ID
         $post = $postRepository->find($id);
 
+        // If the post is not found, throw a not found exception
         if (!$post) {
-            throw $this->createNotFoundException('L\'article demandé n\'existe pas.');
+            throw $this->createNotFoundException('The requested post does not exist.');
         }
 
+        // Render the post details view
         return $this->render('home/show.html.twig', [
             'post' => $post,
         ]);
